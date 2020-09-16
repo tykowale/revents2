c6e8ebdae2186ff16d5fd04696169deacc582212
tykowale <tykowale@gmail.com>
2020-09-16 10:51:51 -0700

    add semantic ui

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Button } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
       <h1>Revents</h1>
+      <button className="ui icon red button">
+        <i className="user icon" /> css button
+      </button>
+      <Button icon="user" content="React Button" color="green" />
     </div>
   );
 }
